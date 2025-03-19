a3ffb3b89c212f2d0a2271e35cca4aa7712ebb3d
K1dou <hique1276@gmail.com>
2025-03-19 16:00:38 -0300

    add project java

---
+++ designpatters/src/main/java/com/kidou/App.java
@@ -0,0 +1,11 @@
+package com.kidou;
+
+import java.util.logging.Logger;
+
+public class App {
+    private static final Logger logger = Logger.getLogger(App.class.getName());
+
+    public static void main(String[] args) {
+        logger.info("Hello World!");
+    }
+}
